642e00045b8a47055f0e5d5537395c1b08264ba2
Rafael Moura <rafaelfelipe.peixoto@gmail.com>
2022-03-18 21:44:31 -0300

    finalização

--- frontend/src/pages/Admin/index.jsx
+++ frontend/src/pages/Admin/index.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../../requests";
+import axios from "axios";
 import NavbarAdm from "../../components/NavbarAdm";
 
-import './styles.css'
+import "./styles.css";
 
 function Admin() {
+  const [destino, setDestino] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/destino/`).then((response) => {
+      const data = response.data;
+      setDestino(data);
+    });
+  }, []);
+  const [hotel, setHotel] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/hotel/`).then((response) => {
+      const data = response.data;
+      setHotel(data);
+    });
+  }, []);
   return (
+    <>
+      <NavbarAdm />
       <div className="admin--section">
-          <NavbarAdm />
+        <div className="card--count--city">
+          <h3>Destinos cadastrados</h3>
+          <h5>{destino.length}</h5>
+        </div>
+        <div className="card--count--hotel">
+          <h3>Hotéis cadastrados</h3>
+          <h5>{hotel.length}</h5>
+        </div>
       </div>
+    </>
   );
 }
 
